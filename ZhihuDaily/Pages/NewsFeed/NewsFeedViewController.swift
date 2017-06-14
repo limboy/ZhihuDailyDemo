70250134e74238f644f9d4bbe791ab4c5e9fc0a2
limboy <limboy.me@gmail.com>
2017-06-14 21:23:57 +0800

    move NewsCell to Components

--- ZhihuDaily/Pages/NewsFeed/NewsFeedViewController.swift
+++ ZhihuDaily/Pages/NewsFeed/NewsFeedViewController.swift
@@ -13,38 +13,6 @@ import RxCocoa
 import Diff
 import ESPullToRefresh
 
-// MARK: NewsCell
-class NewsCell: UITableViewCell {
-    var disposeBag = DisposeBag()
-    
-    override func prepareForReuse() {
-        disposeBag = DisposeBag()
-    }
-    
-    func configure(_ newsItem: NewsItem, favTapHandler: @escaping (UIButton) -> Void) {
-        self.textLabel?.text = newsItem.title
-        
-        let button = UIButton(type: .system)
-        button.frame = CGRect.init(x: 0, y: 0, width: 30, height: 30)
-        
-        if (NewsFeedViewModel.favedNews.value.contains(newsItem)) {
-            button.setTitle("♥︎", for: .normal)
-            button.tag = 1
-        } else {
-            button.setTitle("♡", for: .normal)
-            button.tag = 0
-        }
-        
-        button.rx.tap.asObservable()
-            .subscribe(onNext:{ item in
-                favTapHandler(button)
-            }).addDisposableTo(self.disposeBag)
-        
-        self.accessoryView = button
-    }
-}
-
-
 // MARK: HomeViewController
 class HomeViewController: UIViewController {
     
